1991484d89ab36f8b7106d1af13a2a51254813c2
Srinath Prabhu <srinath@e6x.io>
2023-05-19 00:05:43 +0530

    <Sri>new changes

---
+++ operator/setup.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+# pattern for role name : "[a-zA-Z0-9_\.]{3,64}"
+# pattern for service account name:  [a-zA-Z][a-zA-Z\d\-]*[a-zA-Z\d]
+
+REGION=$1 # Region for infra
+PROJECT_ID=$2  # GCP project ID
+KUBERNETES_OPERATOR_NAMESPACE=$3 # Namespace for e6data operator
+
+if [[ -z "$1" || -z "$2" || -z "$3" ]]; then
+  echo "Usage: ./setup.sh <region> <project_id> <operator_namespace>"
+exit 0
+fi
+
+status_message () {
+  OPERATION=$1
+  COMMAND_CODE=$2
+
+  if [ ${COMMAND_CODE} -ne 0 ]; then
+  echo "${OPERATION}: failed"
+  exit 1
+  fi
+
+  echo "${OPERATION}: Success"
+}
+
+case "$REGION" in
+  northamerica-northeast1|northamerica-northeast2|southamerica-east1|southamerica-west1|us-central1|us-east1|us-east4|us-east5|us-south1|us-west1|us-west2|us-west3|us-west4)
+    LOCATION="us"
+    ;;
+  europe-central2|europe-north1|europe-southwest1|europe-west1|europe-west12|europe-west2|europe-west3|europe-west4|europe-west6|europe-west8|europe-west9)
+    LOCATION="eu"
+    ;;
+  asia-east1|asia-east2|asia-northeast1|asia-northeast2|asia-northeast3|asia-south1|asia-south2|asia-southeast1|asia-southeast2)
+    LOCATION="asia"
+    ;;
+  *)
+    echo "GCP region not supported by e6data. Please contact e6data team for further support"
+    exit 0    
+esac
+
+OPERATOR_NAME="e6data-kubernetes-operator"
+OPERATOR_ROLE_NAME="e6data_kubernetes_operator"
+COMMON_GCP_FLAGS="--project ${PROJECT_ID} --quiet"
+OPERATOR_SA_EMAIL="e6data-kubernetes-operator@${PROJECT_ID}.iam.gserviceaccount.com"
+
+gcloud storage buckets create gs://${OPERATOR_NAME} --location=${LOCATION} ${COMMON_GCP_FLAGS}
+STATUS_CODE=`echo $?`
+status_message "E6DATA_OPERATOR_BUCKET_CREATION" ${STATUS_CODE} 
+
+gcloud iam service-accounts create ${OPERATOR_NAME} --description "Service account for e6data kubernetes operator access" --display-name "${OPERATOR_NAME}" ${COMMON_GCP_FLAGS}
+STATUS_CODE=`echo $?`
+status_message "E6DATA_KUBERNETES_OPERATOR_GCP_SERVICE_ACCOUNT" ${STATUS_CODE} 
+
+# Create role and binding for writer role
+gcloud iam roles create ${OPERATOR_ROLE_NAME} --file gcp_roles/gcs_privileges.yaml ${COMMON_GCP_FLAGS}
+STATUS_CODE=`echo $?`
+status_message "E6DATA_KUBERNETES_OPERATOR_GCP_CUSTOM_ROLE" ${STATUS_CODE} 
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+    --member=serviceAccount:${OPERATOR_SA_EMAIL} \
+    --role=projects/${PROJECT_ID}/roles/${OPERATOR_ROLE_NAME} \
+    --condition="title=${OPERATOR_NAME}-access,description=Full access to ${OPERATOR_NAME} GCS bucket,expression=resource.name.startsWith(\"projects/_/buckets/${OPERATOR_NAME}/\")" \
+    ${COMMON_GCP_FLAGS}
+STATUS_CODE=`echo $?`
+status_message "E6DATA_OPERATOR_IAM_POLICY_BINDING" ${STATUS_CODE}     
+
+# Add workload binding
+gcloud iam service-accounts add-iam-policy-binding ${OPERATOR_SA_EMAIL} \
+    --member "serviceAccount:${PROJECT_ID}.svc.id.goog[${KUBERNETES_OPERATOR_NAMESPACE}/e6data-operator]" \
+    --role roles/iam.workloadIdentityUser \
+    ${COMMON_GCP_FLAGS}
+STATUS_CODE=`echo $?`
+status_message "E6DATA_KUBERNETES_OPERATOR_GSA_KSA_MAPPING" ${STATUS_CODE}   
+
+#bash setup.sh [REGION] [PROJECT_ID] [OPERATOR_NAMESPACE]
+
+
+echo "------------Outputs required for helm script------------"
+echo "E6DATA_OPERATOR_BUCKET_NAME=${OPERATOR_NAME}"
+echo "E6DATA_OPERATOR_GSA_EMAIL=${OPERATOR_SA_EMAIL}"
+echo "--------------------------------------------------------"
---
+++ workspace/setup.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+# pattern for role name : "[a-zA-Z0-9_\.]{3,64}"
+# pattern for service account name:  [a-zA-Z][a-zA-Z\d\-]*[a-zA-Z\d]
+
+REGION=$1 # Region for infra
+PROJECT_ID=$2  # GCP project ID
+WORKSPACE_NAME=$3 # Namespace for workspace 
+CLUSTER_NAME=$4 # Cluster name for the kubernetes
+MAX_INSTANCES_IN_NODEGROUP=$5 # Max VMs to be created in GKE nodegroup
+if [[ -z "$1" || -z "$2" || -z "$3" || -z "$4" || -z "$5" ]]; then
+  echo "Usage: ./setup.sh <region> <project_id> <workspace_name> <cluster_name> <max_nodegroup_instances>"
+exit 0
+fi
+
+status_message () {
+  OPERATION=$1
+  COMMAND_CODE=$2
+
+  if [ ${COMMAND_CODE} -ne 0 ]; then
+  echo "${OPERATION}: failed"
+  exit 1
+  fi
+
+  echo "${OPERATION}: Success"
+}
+
+case "$REGION" in
+  northamerica-northeast1|northamerica-northeast2|southamerica-east1|southamerica-west1|us-central1|us-east1|us-east4|us-east5|us-south1|us-west1|us-west2|us-west3|us-west4)
+    LOCATION="us"
+    ;;
+  europe-central2|europe-north1|europe-southwest1|europe-west1|europe-west12|europe-west2|europe-west3|europe-west4|europe-west6|europe-west8|europe-west9)
+    LOCATION="eu"
+    ;;
+  asia-east1|asia-east2|asia-northeast1|asia-northeast2|asia-northeast3|asia-south1|asia-south2|asia-southeast1|asia-southeast2)
+    LOCATION="asia"
+    ;;
+  *)
+    echo "GCP region not supported by e6data. Please contact e6data team for further support"
+    exit 0    
+esac
+
+WORKSPACE_NAME="e6data-workspace-${WORKSPACE_NAME}"
+WORKSPACE_WRITE_ROLE_NAME="e6data_${WORKSPACE_NAME}_write"
+WORKSPACE_READ_ROLE_NAME="e6data_${WORKSPACE_NAME}_read"
+COMMON_GCP_FLAGS="--project ${PROJECT_ID} --quiet"
+WORKSPACE_SA_EMAIL="e6data-workspace@${PROJECT_ID}.iam.gserviceaccount.com"
+
+
+gcloud container node-pools create ${WORKSPACE_NAME} \
+--cluster=${CLUSTER_NAME} \
+--region=${REGION} \
+--machine-type=c2-standard-30 \
+--enable-autoscaling \
+--total-min-nodes=1 \
+--total-max-nodes=${MAX_INSTANCES_IN_NODEGROUP} \
+--spot \
+--workload-metadata=GKE_METADATA \
+--location-policy=ANY \
+${COMMON_GCP_FLAGS}
+
+gcloud storage buckets create gs://${WORKSPACE_NAME} --location=${LOCATION} ${COMMON_GCP_FLAGS}
+STATUS_CODE=`echo $?`
+status_message "E6DATA_WORKSPACE_BUCKET_CREATION" ${STATUS_CODE}
+
+gcloud iam service-accounts create ${WORKSPACE_NAME} --description "Service account for e6data workspace access" --display-name "${WORKSPACE_NAME}" ${COMMON_GCP_FLAGS}
+STATUS_CODE=`echo $?`
+status_message "E6DATA_WORKSPACE_GCP_SERVICE_ACCOUNT" ${STATUS_CODE} 
+
+find gcp_roles/ -name "*.yaml" -exec sed -i '' "s|dummy|${WORKSPACE_NAME}|g" {} \;
+
+# Create role and binding for writer role
+gcloud iam roles create ${WORKSPACE_WRITE_ROLE_NAME} --file gcp_roles/gcs_write_privileges.yaml ${COMMON_GCP_FLAGS}
+STATUS_CODE=`echo $?`
+status_message "E6DATA_WORKSPACE_GCP_CUSTOM_ROLE" ${STATUS_CODE} 
+
+# Create role and binding for reader role
+gcloud iam roles create ${WORKSPACE_READ_ROLE_NAME} --file gcp_roles/gcs_read_privileges.yaml ${COMMON_GCP_FLAGS}
+STATUS_CODE=`echo $?`
+status_message "E6DATA_WORKSPACE_GCP_CUSTOM_ROLE" ${STATUS_CODE} 
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+    --member=serviceAccount:${WORKSPACE_SA_EMAIL} \
+    --role=projects/${PROJECT_ID}/roles/${WORKSPACE_WRITE_ROLE_NAME} \
+    --condition="title=${WORKSPACE_NAME}-access,description=Write Access to ${WORKSPACE_NAME} GCS bucket,expression=resource.name.startsWith(\"projects/_/buckets/${WORKSPACE_NAME}/\")" \
+    ${COMMON_GCP_FLAGS}
+STATUS_CODE=`echo $?`
+status_message "E6DATA_WORKSPACE_GCS_WRITE_IAM_POLICY_BINDING" ${STATUS_CODE}    
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+    --member=serviceAccount:${WORKSPACE_SA_EMAIL} \
+    --role=projects/${PROJECT_ID}/roles/${WORKSPACE_READ_ROLE_NAME} \
+    --condition="None" \
+    ${COMMON_GCP_FLAGS}
+STATUS_CODE=`echo $?`
+status_message "E6DATA_WORKSPACE_GCS_WRITE_IAM_POLICY_BINDING" ${STATUS_CODE}    
+
+gcloud iam service-accounts add-iam-policy-binding ${WORKSPACE_SA_EMAIL} \
+    --role roles/iam.workloadIdentityUser \
+    --member "serviceAccount:${PROJECT_ID}.svc.id.goog[${WORKSPACE_NAME}/${WORKSPACE_NAME}]" \
+    ${COMMON_GCP_FLAGS}
+STATUS_CODE=`echo $?`
+status_message "E6DATA_WORKSPACE_GSA_KSA_MAPPING" ${STATUS_CODE}     
+
+#bash setup.sh [REGION] [PROJECT_ID] [WORKSPACE_NAME] [CLUSTER_NAME]
+
+
+echo "------------Outputs required for helm script------------"
+echo "GKE_NODEGROUP_NAME=e6data-${WORKSPACE_NAME}"
+echo "GKE_NODEGROUP_MAX_INSTANCES=${MAX_INSTANCES_IN_NODEGROUP}"
+echo "WORKSPACE_GCS_BUCKET_NAME=${WORKSPACE_NAME}"
+echo "E6DATA_WORKSPACE_GSA_EMAIL=${WORKSPACE_SA_EMAIL}"
+echo "E6DATA_WORKSPACE_NAME=${WORKSPACE_NAME}"
+echo "--------------------------------------------------------"
