c81db372c4c5e00953eedf1b4ae96c1a1f297f95
hemant-uniphi <hemant@uniphi.it>
2023-05-19 13:10:03 +0530

    <HS> updated

--- operator/cleanup.sh
+++ operator/cleanup.sh
@@ -1,6 +1,6 @@
 REGION=$1 # Region for infra
 PROJECT_ID=$2  # GCP project ID
-KUBERNETES_OPERATOR_NAMESPACE=$3 # Namespace for e6data operator
+
 
 if [[ -z "$1" || -z "$2" || -z "$3" ]]; then
   echo "Usage: ./setup.sh <region> <project_id> <operator_namespace>"
@@ -14,6 +14,6 @@ OPERATOR_SA_EMAIL="e6data-kubernetes-operator@${PROJECT_ID}.iam.gserviceaccount.
 COMMON_GCP_FLAGS="--project ${PROJECT_ID} --quiet"
 
 
-gsutil rm -r gs://${OPERATOR_NAME}
-gcloud iam service-accounts delete ${OPERATOR_SA_EMAIL}
+gsutil rm -r gs://${OPERATOR_NAME} ${COMMON_GCP_FLAGS}
+gcloud iam service-accounts delete ${OPERATOR_SA_EMAIL} ${COMMON_GCP_FLAGS}
 gcloud iam roles delete ${OPERATOR_ROLE_NAME} ${COMMON_GCP_FLAGS}
--- workspace/cleanup.sh
+++ workspace/cleanup.sh
@@ -7,8 +7,6 @@
 REGION=$1 # Region for infra
 PROJECT_ID=$2  # GCP project ID
 WORKSPACE_NAME=$3 # Namespace for workspace 
-CLUSTER_NAME=$4 # Cluster name for the kubernetes
-MAX_INSTANCES_IN_NODEGROUP=$5 # Max VMs to be created in GKE nodegroup
 
 
 
@@ -20,21 +18,9 @@ WORKSPACE_SA_EMAIL="e6data-workspace@${PROJECT_ID}.iam.gserviceaccount.com"
 
 
 
-if [[ -z "$1" || -z "$2" || -z "$3" || -z "$4" || -z "$5" ]]; then
-  echo "Usage: ./setup.sh <region> <project_id> <workspace_name> <cluster_name> <max_nodegroup_instances>"
-exit 0
-fi
-
-
-if [[ -z "$1" || -z "$2" || -z "$3" ]]; then
-  echo "Usage: ./setup.sh <region> <project_id> <operator_namespace>"
-exit 0
-fi
-
-
-
-gsutil rm -r gs://${WORKSPACE_NAME}
-gcloud iam service-accounts delete ${WORKSPACE_NAME}
+gsutil rm -r gs://${WORKSPACE_NAME} ${COMMON_GCP_FLAGS}
+gcloud iam service-accounts delete ${WORKSPACE_NAME} ${COMMON_GCP_FLAGS}
 gcloud iam roles delete ${WORKSPACE_WRITE_ROLE_NAME} ${COMMON_GCP_FLAGS}
+gcloud iam roles delete ${WORKSPACE_READ_ROLE_NAME} ${COMMON_GCP_FLAGS}
 gcloud container node-pools delete ${WORKSPACE_NAME} --cluster=${CLUSTER_NAME} --region=${REGION}
 
