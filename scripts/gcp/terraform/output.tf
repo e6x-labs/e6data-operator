f4231a990aa3b26d253d416c3b6d7df39bd3a112
Harshith <104987110+Harshithraj24@users.noreply.github.com>
2023-07-06 08:36:42 +0530

    <harshith>added terraform scripts for gcp

--- scripts/gcp/terraform/output.tf
+++ scripts/gcp/terraform/output.tf
@@ -1,23 +1,23 @@
-output "GKE_NODEGROUP_NAME" {
+output "gke_nodegroup_name" {
   value = local.workspace_namespace
 }
 
-output "GKE_NODEGROUP_MAX_INSTANCES" {
+output "gke_nodegroup_max_instances" {
   value = var.max_instances_in_nodegroup
 }
 
-output "WORKSPACE_GCS_BUCKET_NAME" {
+output "workspace_gcs_bucket_name" {
   value = local.workspace_namespace
 }
 
-output "E6DATA_WORKSPACE_GSA_EMAIL" {
+output "e6data_workspace_gsa_email" {
   value = local.workspace_sa_email
 }
 
-output "E6DATA_WORKSPACE_NAME" {
+output "e6data_workspace_name" {
   value = var.workspace_name
 }
 
-output "KUBERNETES_NAMESPACE" {
+output "kubernetes_namespace" {
   value = var.kubernetes_namespace
 }
