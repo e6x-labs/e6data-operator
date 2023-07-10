f221ac88c1363adbc16a3838c110381c4a720440
Harshith <104987110+Harshithraj24@users.noreply.github.com>
2023-07-10 16:20:23 +0530

    PLT-2483 <harshith>added azure terraform

--- scripts/azure/terraform/e6data_engine_iam.tf
+++ scripts/azure/terraform/e6data_engine_iam.tf
@@ -1,12 +1,12 @@
 # role assignment to provide permission to service principal to read the data in the containers
-resource "azurerm_role_assignment" "e6data_identity_read_role" {
+resource "azurerm_role_assignment" "e6data_engine_read_role" {
   for_each             = var.list_of_containers != ["*"] ? toset(var.list_of_containers) : [""]
   scope                = each.value != "" ? "/subscriptions/${var.subscription_id}/resourceGroups/${azurerm_resource_group.e6data_rg.name}/providers/Microsoft.Storage/storageAccounts/${azurerm_storage_account.e6data_storage_account.name}/blobServices/default/containers/${each.value}" : "/subscriptions/${var.subscription_id}/resourceGroups/${azurerm_resource_group.e6data_rg.name}/providers/Microsoft.Storage/storageAccounts/${azurerm_storage_account.e6data_storage_account.name}"
   role_definition_name = "Storage Blob Data Reader"
   principal_id         = azurerm_user_assigned_identity.e6data_identity.principal_id
 }
 # managed identity role assignment to provide read and write access to the e6data managed storage account
-resource "azurerm_role_assignment" "e6data_identity_write_role" {
+resource "azurerm_role_assignment" "e6data_engine_write_role" {
   scope                = azurerm_storage_account.e6data_storage_account.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_user_assigned_identity.e6data_identity.principal_id
